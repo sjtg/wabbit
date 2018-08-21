937466124c802bd1648364aeb7b80e80c0af70cc
Joshua Chipile <chipilej@gmail.com>
2018-08-21 16:30:02 +0200

    added card section on home page

--- js/init.js
+++ js/init.js
@@ -21,6 +21,11 @@
     $('html, body').animate({
       scrollTop: $(".intro").offset().top
     }, 2000);
+    $('.carousel-slider').carousel({fullWidth: true, padding:0},setTimeout(autoplay, 4500));
+function autoplay() {
+  $('.carousel').carousel('next');
+  setTimeout(autoplay, 7500);
+   }
 
 
 // $('.select').material_select();
